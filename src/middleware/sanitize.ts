abbfff13edc4973669d849d4bf0049b43c4443d8
CePCCo-Asesores <86344452+CePCCo-Asesores@users.noreply.github.com>
2025-09-14 18:52:14 -0600

    Create sanitize.ts

---
+++ src/middleware/sanitize.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const sanitizeInput = (req: Request, _: Response, next: NextFunction) => {
+  for (const key in req.body) {
+    if (typeof req.body[key] === 'string') {
+      req.body[key] = req.body[key].replace(/[<>]/g, '');
+    }
+  }
+  next();
+};
