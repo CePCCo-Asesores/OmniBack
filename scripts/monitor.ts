fe56e105304f06ef9919890f3b407b0ba32819c0
CePCCo-Asesores <86344452+CePCCo-Asesores@users.noreply.github.com>
2025-09-14 19:18:04 -0600

    Delete scripts directory

--- scripts/monitor.ts
+++
@@ -1,5 +0,0 @@
-import { listInstances } from '../src/dashboard/instances';
-import { getAllUsers } from '../src/dashboard/users';
-
-console.log('Instancias activas:', listInstances());
-console.log('Usuarios registrados:', getAllUsers().length);
