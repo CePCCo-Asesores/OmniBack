e60972fa98f4c11c025239ab88b36195528678ea
CePCCo-Asesores <86344452+CePCCo-Asesores@users.noreply.github.com>
2025-09-14 18:40:16 -0600

    Update connection.ts

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,5 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
