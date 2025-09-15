ca29deeed5448ff84476f41e30b20154c450de5e
CePCCo-Asesores <86344452+CePCCo-Asesores@users.noreply.github.com>
2025-09-14 18:55:44 -0600

    Delete server.ts

--- server.ts
+++
@@ -1,7 +0,0 @@
-import { limiter } from './middleware/rateLimiter';
-import { sanitizeInput } from './middleware/sanitize';
-import { corsConfig } from './middleware/cors';
-
-app.use(limiter);
-app.use(sanitizeInput);
-app.use(corsConfig);
